f295f96b77a0bf232103812fe33e239b7c13ca5c
Dean Hamstead <dean@fragfest.com.au>
2011-10-06 09:46:06 +1100

    deploy.pl: cleaned up

--- deploy.pl
+++ deploy.pl
@@ -1,4 +1,11 @@
-## 
+#!/usr/bin/perl
+##
+## in debian and its family...
+## apt-get install libhttp-message-perl
+## apt-get install libwww-perl
+## apt-get install libjson-perl
+##
+## if desperate
 ## perl -MCPAN -e 'install HTTP::Request::Common'
 ## perl -MCPAN -e 'install Bundle::LWP'
 ## perl -MCPAN -e 'install JSON'
@@ -6,33 +13,48 @@
 ##
 ## Based on Jason Greene's AS7 perl video
 
+use strict;
+use warnings;
+use Carp;
+
 use HTTP::Request::Common;
 use LWP::UserAgent;
 use JSON;
 
-$filename = 'node-info.war';
-$deploymentname = 'node-info.war';
+my $filename       = 'node-info.war';
+my $deploymentname = 'node-info.war';
 
-$ua = LWP::UserAgent->new;
+my $ua = LWP::UserAgent->new();
 
-$resp = $ua->request(POST 'http://localhost:9990/management/add-content',
-                                    Content_Type => 'form-data',
-                                    Content => [file => [$filename, Content_Type => "application/octet-stream"]]);
+my $resp = $ua->request(
+    POST 'http://localhost:9990/management/add-content',
+    Content_Type => 'form-data',
+    Content =>
+      [ file => [ $filename, Content_Type => 'application/octet-stream' ] ]
+);
 
-die $resp->status_line if ! $resp->is_success;
+croak $resp->status_line
+  unless $resp->is_success;
 
-$json = JSON->new->allow_nonref;
+my $json = JSON->new()->allow_nonref();
 
-$hash = \%{$json->decode($resp->decoded_content)->{'result'}};
+my $hashref = $json->decode( $resp->decoded_content )->{'result'};
 
-print "The uploaded hash is " . $hash->{'BYTES_VALUE'} . "\n";
+print 'The uploaded hash is ', $hashref->{BYTES_VALUE}, "\n";
 
-$deploy = $json->encode({"operation" => "add",
-	  			     	    "address" => [{"deployment" => $deploymentname}],
-                                            "enabled" => "true",
-                                            "content" => [{hash => $hash}]});
+my $deploy = $json->encode(
+    {
+        'operation' => 'add',
+        'address'   => [ { 'deployment' => $deploymentname } ],
+        'enabled'   => 'true',
+        'content'   => [ { hash => $hashref } ]
+    }
+);
 
-$resp = $ua->request(POST 'http://localhost:8888/management', Content=>$deploy);
+$resp =
+  $ua->request( POST 'http://localhost:8888/management', Content => $deploy );
 
-die $resp->status_line if ! $resp->is_success;
+croak $resp->status_line
+  unless $resp->is_success;
+
 print "Success!!!\n"
